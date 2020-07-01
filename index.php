f784d85d4ef4994989f26c435dd3789be29ea477
LFCRab <56073739+LFCRab@users.noreply.github.com>
2020-07-01 11:57:54 +0100

    Small phone edit

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
                 alongside a comment!</p>
         </header>
         <form>
+            
             <input type="text" class="todo-input">
             <button class="todo-button" type="submit">
                 <i class="fas fa-plus-square"></i>
