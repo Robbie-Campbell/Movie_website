5a2db8be638241d03b6211607622da20d7bcc216
LFCRab <56073739+LFCRab@users.noreply.github.com>
2020-07-01 12:11:16 +0100

    Removed filter todo

--- app.js
+++ app.js
@@ -2,13 +2,11 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
-const filterOption = document.querySelector(".filter-todo");
 
 //event listeners
 document.addEventListener("DOMContentLoaded", getTodos);
 todoButton.addEventListener('click', addToDo);
 todoList.addEventListener('click', deleteCheck);
-filterOption.addEventListener('click', filterTodo);
 //functions
 function addToDo(event){
     //Prevent form from submitting
@@ -59,37 +57,6 @@ function deleteCheck(e){
         const todo = item.parentElement;
         todo.classList.toggle("completed");
     }
-}
-
-function filterTodo(e){
-    const todos = todoList.childNodes;
-    todos.forEach(function(todo){
-        switch(e.target.value)
-        {
-        case "all":
-            todo.style.display = "flex";
-            break;
-        case "completed":
-            if (todo.classList.contains("completed")){
-                todo.style.display = 'flex';
-            }
-            else{
-                todo.style.display = "none";
-            }
-            break;
-        case "uncompleted":
-            if (!todo.classList.contains("completed")){
-                todo.style.display = 'flex';
-            }
-            else{
-                todo.style.display = "none";
-            }
-            break;
-        }
-
-    });
-    
-    
 }
 
 function saveLocalTodos(todo){
